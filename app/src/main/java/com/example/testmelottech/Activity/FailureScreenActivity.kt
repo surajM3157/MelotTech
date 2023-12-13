a97cd4ef84a0f4187b27b408fa10059e8798ed3e
SurajMaurya <surajm315760@gmail.com>
2023-12-13 23:05:00 +0530

    Commit payment new Activity

---
+++ app/src/main/java/com/example/testmelottech/Activity/FailureScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.example.testmelottech.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.testmelottech.R
+
+class FailureScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_failure_screen)
+    }
+}
