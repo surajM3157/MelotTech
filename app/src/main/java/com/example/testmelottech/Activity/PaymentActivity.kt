4dccd90947e01eacd53d8c3a8404336a267eb3cc
SurajMaurya <surajm315760@gmail.com>
2023-12-13 10:38:26 +0530

    First Commit Code All Branch

---
+++ app/src/main/java/com/example/testmelottech/Activity/PaymentActivity.kt
@@ -0,0 +1,12 @@
+package com.example.testmelottech.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.testmelottech.R
+
+class PaymentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_payment_phone_pay)
+    }
+}
